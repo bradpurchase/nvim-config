8123ac400878da3c6dc50be3f9232466bb4e00d5
Brad Purchase <bradpurchase@gmail.com>
2025-12-13 09:58:04 -0500

    use standardrb instead of rubocop, disable mason-lspconfig
    mason-lspconfig was causing ruby_lsp to be loaded twice, and I'd prefer
    to manually control LSP servers

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,11 +1,25 @@
 return {
   {
+    "mason-org/mason-lspconfig.nvim",
+    enabled = false,
+  },
+  {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
         ruby_lsp = {
           mason = false,
           cmd = { vim.fn.expand("~/.asdf/shims/ruby-lsp") },
+          init_options = {
+            formatter = "standardrb",
+            linters = { "standardrb" },
+          },
+        },
+        rubocop = {
+          enabled = false,
+        },
+        standardrb = {
+          enabled = "standardrb",
         },
       },
     },
