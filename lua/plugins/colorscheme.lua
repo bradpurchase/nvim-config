ec01d1e4f68e6206df70639e864c1f6a4f405c1a
Brad Purchase <5716+bradpurchase@users.noreply.github.com>
2026-01-03 09:44:01 -0330

    add colorschemes and configure telescope color picker

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,13 +4,29 @@ return {
   { "folke/tokyonight.nvim" },
   { "catppuccin/nvim" },
   { "rose-pine/neovim" },
+  { "AlexvZyl/nordic.nvim" },
+  { "sainnhe/gruvbox-material" },
+  { "EdenEast/nightfox.nvim" },
 
   -- Configure LazyVim to load color scheme
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "kanagawa-wave",
+      -- colorscheme = "nordic",
       -- colorscheme = "catppuccin",
+    },
+  },
+
+  -- Configure Telescope colorscheme picker
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      {
+        "<leader>uC",
+        "<cmd>Telescope colorscheme enable_preview=true<cr>",
+        desc = "Colorscheme with preview",
+      },
     },
   },
 }
